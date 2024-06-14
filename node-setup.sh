54c154c46465fc7e217845c31b4dd609e9fa1b82
PoodlBlockchain <poodlofficial@gmail.com>
2024-06-14 10:41:23 +0530

    fixed & improve node-setup.sh

--- node-setup.sh
+++ node-setup.sh
@@ -176,6 +176,7 @@ createRpc(){
     ./node_src/build/bin/geth --datadir ./chaindata/node$i init ./genesis.json
     ((i += 1))
   done
+
 }
 
 createValidator(){
@@ -200,6 +201,48 @@ finalize(){
   createValidator
   labelNodes
   #fetchNsetIP
+
+
+  # resource paths
+  nodePath=/root/Core-Blockchain
+  ipcPath=$nodePath/chaindata/node1/geth.ipc
+  chaindataPath=$nodePath/chaindata/node1/geth
+  snapshotName=$nodePath/chaindata.tar.gz
+
+
+  # Make sure that the node installation is found, else exit
+
+  if [ !(-e "$chaindataPath") ]; then
+	  echo -e "\n\n\t${RED}A node installation was not found. Exiting...${NC}"
+    exit 1
+  fi
+
+  # stop the node incase the node is running
+  ./node_src/build/bin/geth --exec "exit" attach ipc:$ipcPath
+
+  echo -e "\n\t${ORANGE}Sleeping for 15 seconds so that the node can shutdown gracefully"
+  sleep 15
+
+  echo -e "\n\n\t${ORANGE}Removing existing chaindata, if any${NC}"
+  rm $ipcPath
+  rm -rf $chaindataPath/chaindata
+
+  echo -e "\n\n\t${GREEN}Now importing the snapshot"
+  wget https://snapshots.poodl.org/chaindata.tar.gz
+  tar -xvf $snapshotName $chaindataPath
+
+  echo -e "\n\n\tImport is done, now attempting to run the node${NC}"
+  sleep 3
+  cd $nodePath
+  if [ -e "$nodePath/chaindata/node1/.validator" ]; then
+    type=validator
+  else
+    type=rpc
+  fi
+
+  ./node-start.sh --$type
+
+
   displayStatus
 }
 
